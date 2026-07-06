54f38b85618e60f2804fb2e480eb186c84259209
1023am0645-pixel <1023am0645@gmail.com>
2026-07-06 21:30:54 +0900

    Add configuration for event app

---
+++ config.js
@@ -0,0 +1,7 @@
+window.EVENT_APP_CONFIG = {
+    supabaseUrl: "https://hxupaazboxhbhzoqfxnn.supabase.co",
+    supabaseAnonKey: "sb_publishable_dxVqQK3WmnKRUdXqy-DSvQ_3BHauKjZ",
+    tableName: "photo_event_2026_h1",
+    bucketName: "photo-contest-2026-h1",
+    maxUploadBytes: 12 * 1024 * 1024
+};
